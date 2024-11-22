fc0c89c4ebef6fb4518ee6d4dee44dbc5e8f5d81
SuriNNAT <157988856+SuriNNAT@users.noreply.github.com>
2024-11-22 16:30:29 -0500

    Update HW3.cpp

--- HW3.cpp
+++ HW3.cpp
@@ -1,20 +1,28 @@
-﻿// HW3.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#include<cstdlib>
 #include <iostream>
+#include <ctime>
+#include <unordered_set>
 
+using namespace std;
+
+int randomNumber() {
+    return 1 + (rand() % (1000));
+}
 int main()
 {
-    std::cout << "Hello World!\n";
+    //initialize the array
+    int dataset[100];
+    unordered_set<int> unique_numbers;
+
+    //generate the number and save to the array
+    srand(time(0));
+    for (int i = 0; i < 100; ) {
+        int num = randomNumber();
+        if (unique_numbers.find(num) == unique_numbers.end()) {
+            dataset[i] = num;
+            unique_numbers.insert(num);
+            ++i;
+        }
+    }
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
