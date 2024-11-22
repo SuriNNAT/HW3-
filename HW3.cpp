0fd02bf43ce135fdf99b286fd1a02f0990216701
heweytheduck <huynhhy@mail.uc.edu>
2024-11-22 16:49:15 -0500

    Update HW3.cpp
    insert

--- HW3.cpp
+++ HW3.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <ctime>
 #include <unordered_set>
+#include "HashTable1D.h"
+#include "HashTable2D.h"
 
 using namespace std;
 
@@ -14,6 +16,10 @@ int main()
     int dataset[100];
     unordered_set<int> unique_numbers;
 
+    //hash tables
+    HashTable1D hash1(500);
+    HashTable2D hash2(500);
+
     //generate the number and save to the array
     srand(time(0));
     for (int i = 0; i < 100; ) {
@@ -24,5 +30,14 @@ int main()
             ++i;
         }
     }
+
+    //1st insertion
+    int insert_1 = 0;
+    int insert_2 = 0;
+    for (int i = 0; i < 50; i++) {
+        insert_1 += hash1.insert(dataset[i]);
+        insert_2 += hash2.insert(dataset[i]);
+    }
+    cout << insert_1 << " " << insert_2;
 
 }
